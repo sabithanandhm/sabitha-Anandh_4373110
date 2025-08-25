41d15c92beacdcfeaa4e72aea790a05ba0c16e63
sabithanandhm <Sabithanandhm@gmail.com>
2025-08-25 14:57:00 +0530

    Add files via upload

---
+++ 25.08.2025/cascading.sql
@@ -0,0 +1,32 @@
+show databases;
+create database collage;
+use collage;
+CREATE TABLE Department(
+    DeptID INT PRIMARY KEY,
+    DeptName VARCHAR(50)
+);
+CREATE TABLE Employees (
+    EmpID INT PRIMARY KEY,
+    EmpName VARCHAR(50),
+    DeptID INT,
+        FOREIGN KEY (DeptID) REFERENCES Department(DeptID)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+
+INSERT INTO Department(DeptID, DeptName)
+VALUES (1001,'HR'),
+       (1002,'Finance'),
+       (1003,'IT');
+
+INSERT INTO Employees(EmpID, EmpName, DeptID)
+VALUES (43731110,'Sabitha',1001),
+       (43731130,'Sivapriya',1002),
+       (43731098,'Prince',1003);
+select*from department;
+select*from Employees;
+DELETE FROM Department WHERE DeptID = 1003;
+UPDATE Department
+SET DeptID = 2002
+WHERE DeptID = 1002;
