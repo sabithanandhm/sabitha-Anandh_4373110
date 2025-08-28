f52998dc0a0a56cbcfc869998fd9c195938ffb16
sabithanandhm <Sabithanandhm@gmail.com>
2025-08-28 15:12:56 +0530

    Add files via upload

---
+++ 28.08.2025/having.sql
@@ -0,0 +1,34 @@
+
+CREATE DATABASE ORG123;
+SHOW DATABASES;
+USE ORG123;
+
+CREATE TABLE Worker (
+	WORKER_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	FIRST_NAME CHAR(25),
+	LAST_NAME CHAR(25),
+	SALARY INT(15),
+	JOINING_DATE DATETIME,
+	DEPARTMENT CHAR(25)
+);
+
+INSERT INTO Worker 
+	(WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT) VALUES
+		(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
+		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
+		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
+		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
+		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
+		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
+		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
+		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
+select  department, avg(salary) from worker group by department
+having avg(salary)>100000;
+select department , MAX(salary) from worker group by department
+having  max(salary)=500000;
+select  department ,count(*) ,sum(salary) from worker group by department having count(*) >1 and sum(salary) >100000;
+select department ,avg(salary) from worker group by department having avg(salary) BETWEEN 50000 AND  200000; 
+select  FIRST_NAME, avg(salary) from worker group by FIRST_NAME
+having avg(salary)>100000;
+select LAST_NAME ,avg(salary) from worker group by LAST_NAME having avg(salary) BETWEEN 50000 AND  200000; 
+
---
+++ 28.08.2025/subqueries.sql
@@ -0,0 +1,28 @@
+
+CREATE DATABASE ORG123;
+SHOW DATABASES;
+USE ORG123;
+
+CREATE TABLE Worker (
+	WORKER_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	FIRST_NAME CHAR(25),
+	LAST_NAME CHAR(25),
+	SALARY INT(15),
+	JOINING_DATE DATETIME,
+	DEPARTMENT CHAR(25)
+);
+
+INSERT INTO Worker 
+	(WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT) VALUES
+		(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
+		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
+		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
+		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
+		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
+		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
+		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
+		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
+select* from  worker where Salary not in( SELECT salary FROM worker where FIRST_NAME='VISHAL');
+select* from  worker where Salary  in( SELECT salary FROM worker where FIRST_NAME='monika');
+select* from worker where JOINING_DATE in(select JOINING_DATE from worker where LAST_NAME='diwan');
+         
