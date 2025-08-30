fca0552e53af273d2694d5999861203138c09ebb
sabithanandhm <Sabithanandhm@gmail.com>
2025-08-30 11:44:58 +0530

    Add files via upload

---
+++ 30.08.2025/java using.sql
@@ -0,0 +1,8 @@
+create database saturday;
+use saturday;
+create table emp(
+id int,
+name varchar(35),
+age int);
+insert into emp values (001,'sabi',18),(002,'akku',20),(003,'shreya',21);
+select* from emp;
